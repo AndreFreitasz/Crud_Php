d9edb7f85bb55021a87149b1765c7d2ede99da2d
André Freitas <andre.filho@kabum.com.br>
2024-01-29 14:57:16 -0300

    [wip] formulário endereço

--- Pages/NewClient/newClient.php
+++ Pages/NewClient/newClient.php
@@ -9,22 +9,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <style>
         .nav-tabs .nav-link.active {
-            background-color: #ff7b00;
-            color: #fff;
+            color: #ff7b00;
             border: 3px solid #ff7b00;
         }
 
         .nav-tabs .nav-link {
             background-color: #fff;
-            border: 3px solid #ff7b00;
             color: #ff7b00;
             margin-right: 2rem;
             padding-left: 3rem;
             padding-right: 3rem;
-        }
-
-        .nav-tabs .nav-link:hover {
-            border: 3px solid #ff7b00;
         }
 
         .nav-tabs {
@@ -34,6 +28,18 @@
         .tab-content {
             border-bottom: 1px solid #ff7b00;
             padding: 20px;
+        }
+
+        input[name="submitClient"] {
+            background-color: #ff7b00;
+            display: flex;
+            margin-top: 16px;
+        }
+
+        input[name="submitAddress"] {
+            background-color: #ff7b00;
+            display: flex;
+            margin-top: 16px;
         }
     </style>
 </head>
@@ -46,14 +52,6 @@
                     <input type="submit" name="back" value="Voltar para home">
                 </div>
             </form>
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                <input type="submit" name="logout" value="Sair">
-
-                <?php
-                if (isset($_POST["logout"])) {
-                    logout();
-                }
-                ?>
         </div>
     </header>
 
@@ -81,58 +79,104 @@
                 <div class="row border g-0 rounded shadow-sm">
                     <div class="col p-4">
                         <h3>Cliente</h3>
-                        <form>
+                        <form action="newClient.php" method="POST">
                             <div class="row">
                                 <div class="col-md-6 mt-4 mb-3">
                                     <label for="nameClient" class="form-label">Nome: *</label>
-                                    <input type="text" class="form-control" id="nameClient" aria-describedby="nameClient" placeholder="Nome completo" required>
+                                    <input type="text" name="nameClient" class="form-control" id="nameClient" aria-describedby="nameClient" placeholder="Nome completo" required>
                                 </div>
                                 <div class="col-md-6 mt-4 mb-3">
                                     <label for="emailClient" class="form-label">E-mail: *</label>
-                                    <input type="email" class="form-control" id="emailClient" aria-describedby="emailHelp" placeholder="Seu e-mail" required>
+                                    <input type="email" name="emailClient" class="form-control" id="emailClient" aria-describedby="emailHelp" placeholder="Seu e-mail" required>
                                 </div>
                             </div>
 
                             <div class="row">
                                 <div class="col-md-6 mt-4 mb-3">
                                     <label for="cpfClient" class="form-label">CPF: *</label>
-                                    <input type="text" class="form-control" id="cpfClient" aria-describedby="cpf" placeholder="CPF" required>
+                                    <input type="text" name="cpfClient" class="form-control" id="cpfClient" aria-describedby="cpf" placeholder="CPF" required>
                                 </div>
                                 <div class="col-md-6 mt-4 mb-3">
                                     <label for="rgClient" class="form-label">RG: *</label>
-                                    <input type="text" class="form-control" id="rgClient" aria-describedby="rg" placeholder="RG" required>
+                                    <input type="text" name="rgClient" class="form-control" id="rgClient" aria-describedby="rg" placeholder="RG" required>
                                 </div>
                             </div>
 
                             <div class="row">
                                 <div class="col-md-4 mt-4 mb-3">
                                     <label for="telephoneClient" class="form-label">Telefone: *</label>
-                                    <input type="text" class="form-control" id="telephoneClient" aria-describedby="telephone" placeholder="Telefone Principal" required>
+                                    <input type="text" name="telephoneClient" class="form-control" id="telephoneClient" aria-describedby="telephone" placeholder="Telefone Principal" required>
                                 </div>
                                 <div class="col-md-4 mt-4 mb-3">
                                     <label for="optionalTelephone" class="form-label">Telefone 2: </label>
-                                    <input type="text" class="form-control" id="optionalTelephone" aria-describedby="optionalTelephone" placeholder="Telefone Adcional" required>
+                                    <input type="text" class="form-control" name="optionalTelephone" id="optionalTelephone" aria-describedby="optionalTelephone" placeholder="Telefone Adcional">
                                 </div>
                                 <div class="col-md-4 mt-4 mb-3">
                                     <label for="dateOfBirth" class="form-label">Data de Nascimento: *</label>
-                                    <input type="date" class="form-control" id="dateOfBirth" aria-describedby="date" placeholder="Data de Nascimento">
+                                    <input type="date" class="form-control" name="dateOfBirth" id="dateOfBirth" aria-describedby="date" placeholder="Data de Nascimento" required>
                                 </div>
                             </div>
-                        </form>
-                    </div>
-                    <div class="col-auto">
 
+                            <input type="submit" name="submitClient" value="Salvar Alterações" />
+
+                        </form>
                     </div>
                 </div>
             </div>
+
             <div class="tab-pane" id="address">
                 <div class="row border g-0 rounded shadow-sm">
                     <div class="col p-4">
-                        <h3>Endereços</h3>
-                        <p>fgvnjfdoughjdfioughdfiughdfiug</p>
-                    </div>
-                    <div class="col-auto">
+                        <div class="row">
+                            <div class="col-md-9 mt-4 mb-3">
+                                <h3>Endereços</h3>
+                            </div>
+                            <div class="col-md-3 mt-4 mb-3">
+                                <button type="button" onclick="adicionarCampo()" class="addAddress"> Adicionar endereço</button>
+                            </div>
+                        </div>
+                        <form method="POST" action="clients.php">
+                            <div id="enderecos">
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="col-md-6 mt-4 mb-3">
+                                            <label class="form-label" for="cep">CEP: </label>
+                                            <input type="text" class="form-control" name="cep[]" id="cep" placeholder="CEP" aria-describedby="cep" required />
+                                        </div>
+                                        <div class="col-md-6 mt-4 mb-3">
+                                            <label>Rua: </label>
+                                            <input type="text" class="form-control" aria-describedby="rua" name="rua[]" id="rua" placeholder="Rua" />
+                                        </div>
+                                    </div>
 
+                                    <div class="row">
+                                        <div class="col-md-6 mt-4 mb-3">
+                                            <label>Número: </label>
+                                            <input type="number" class="form-control" aria-describedby="numero" name="numero[]" id="numero" placeholder="Número" />
+                                        </div>
+                                        <div class="col-md-6 mt-4 mb-3">
+                                            <label>Bairro: </label>
+                                            <input type="text" class="form-control" aria-describedby="bairro" name="bairro[]" id="bairro" placeholder="Bairro" />
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-6 mt-4 mb-3">
+                                            <label>Estado: </label>
+                                            <input type="text" class="form-control" aria-describedby="estado" name="estado[]" id="estado" placeholder="Estado" />
+                                        </div>
+                                        <div class="col-md-6 mt-4 mb-3">
+                                            <label>Cidade: </label>
+                                            <input type="text" class="form-control" aria-describedby="cidade" required name="cidade[]" id="cidade" placeholder="Cidade" />
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <div class="form-group">
+                                <input type="submit" name="submitAddress" value="Salvar Alterações" />
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -142,8 +186,7 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/inputmask@5/dist/jquery.inputmask.min.js"></script>
-
-        <script>
+        <script src="js/address.js" <script>
             $(document).ready(function() {
 
                 // Máscara para CPF
